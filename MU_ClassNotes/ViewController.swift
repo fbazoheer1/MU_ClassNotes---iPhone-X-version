bf7fdbecef3dec1f22e409895bf90ee37cd4ee69
fbazoheer1 <fbazoheer1@live.maryville.edu>
2018-01-31 16:47:37 -0600

    Initial Commit

---
+++ MU_ClassNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MU_ClassNotes
+//
+//  Created by Faris Bazoheer on 1/31/18.
+//  Copyright © 2018 Faris Bazoheer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
